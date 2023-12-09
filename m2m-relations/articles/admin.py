85911ad87af9a9a963e0644c28176dddcd61716c
Egor <egormakeich@yandex.ru>
2023-12-09 14:46:04 +1000

    first commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django import forms
+
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(forms.BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                count += 1
+            if count > 1:
+                raise forms.ValidationError('Выберите один основной тег')
+        if count == 0:
+            raise forms.ValidationError('Выберите основной тег')
+
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 3
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+admin.site.register(Tag)
